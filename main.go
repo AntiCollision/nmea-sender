85822dbc8861d9cc4b7ea2fb8d7dce707b2c6510
Piorosen <aoikazto@naver.com>
2023-07-14 17:11:38 +0900

    코드 구현함

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 
+	"github.com/adrianmo/go-nmea"
 	"github.com/tarm/serial"
 )
 
@@ -14,6 +16,7 @@ func main() {
 	baudRate := flag.Int("b", 115200, "Baudrate 정보를 입력해 주세요.")
 	sendIP := flag.String("i", "", "전송할 IP 정보를 입력해 주세요.")
 	sendPort := flag.Int("p", 0, "전송할 Port 정보를 입력해 주세요.")
+	flag.Parse()
 
 	fmt.Println("ComPort : ", *comPort)
 	fmt.Println("Baudrate : ", *baudRate)
@@ -22,18 +25,24 @@ func main() {
 
 	c := &serial.Config{Name: *comPort, Baud: *baudRate}
 	s, err := serial.OpenPort(c)
+	read := bufio.NewScanner(s)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer s.Close()
-	buf := make([]byte, 1024)
 
-	n, err := s.Read(buf)
-	if err != nil {
-		log.Fatal(err)
+	for {
+		if read.Scan() {
+			text := read.Text()
+			nm, err := nmea.Parse(text)
+			if err != nil {
+				log.Println(err)
+			} else {
+				log.Println(nm)
+			}
+		}
 	}
-	log.Printf("%q", buf[:n])
-
 	_, err = net.Dial("tcp", fmt.Sprintf("%s:%d", *sendIP, *sendPort))
 
 	if err != nil {
